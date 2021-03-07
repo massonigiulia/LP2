0e1c9016a3a575b5435abae535e49c5b414d837a
Giulia Alves Massoni <59745030+massonigiulia@users.noreply.github.com>
2021-03-07 12:57:21 -0300

    Add PTriangulos

---
+++ Atividade 3/PTriangulos/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace PTriangulos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(297, 259);
+            this.txtC.Margin = new System.Windows.Forms.Padding(4);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(148, 24);
+            this.txtC.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 262);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Lado C:";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(485, 167);
+            this.txtB.Margin = new System.Windows.Forms.Padding(4);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(148, 24);
+            this.txtB.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(423, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 18);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Lado B:";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(139, 167);
+            this.txtA.Margin = new System.Windows.Forms.Padding(4);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(148, 24);
+            this.txtA.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 171);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Lado A:";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(426, 510);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(112, 32);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(150, 510);
+            this.btnLimpar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(112, 32);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(285, 422);
+            this.btnVerificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(112, 32);
+            this.btnVerificar.TabIndex = 9;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 61);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(698, 18);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Insira o tamanho dos lados para verificar se podem pertencer a um triângulo e qua" +
+    "l tipo de triângulo ele é.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(717, 585);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Triângulos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Atividade 3/PTriangulos/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PTriangulos
+{
+    public partial class Form1 : Form
+    {
+        double ladoA, ladoB, ladoC;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+            txtA.Focus();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            //verificar se são números
+            if (double.TryParse(txtA.Text, out ladoA) && double.TryParse(txtB.Text, out ladoB) && double.TryParse(txtC.Text, out ladoC))
+            {
+                //verificar se podem ser lados de um triângulo
+                if ((ladoB - ladoC) < ladoA && ladoA < ladoB + ladoC && (ladoA - ladoC) < ladoB && ladoB < ladoA + ladoC &&(ladoA - ladoB) < ladoC && ladoC < ladoA + ladoB)
+                {
+                    if ((ladoA == ladoB) && (ladoC == ladoB))
+                    {
+                        MessageBox.Show("Equilátero.");
+                    }
+                    else
+                    {
+                        if ((ladoA != ladoB) && (ladoB != ladoC) && (ladoC != ladoA))
+                        {
+                            MessageBox.Show("Escaleno.");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Isósceles.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Valores inválidos!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Digite apenas números!");
+            }
+        }
+    }
+}
